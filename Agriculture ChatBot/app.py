dec391d1b3e2c37534324d093f2fc19662c0724f
Dhyaan Dharmesh Kanoja <135312153+DhyaanKanoja11@users.noreply.github.com>
2026-02-13 19:49:36 +0530

    Update app.py

--- Agriculture ChatBot/app.py
+++ Agriculture ChatBot/app.py
@@ -197,4 +197,5 @@ def chat():
 if __name__ == '__main__':
     os.makedirs("uploads", exist_ok=True)
     os.makedirs("static/audio", exist_ok=True)
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
